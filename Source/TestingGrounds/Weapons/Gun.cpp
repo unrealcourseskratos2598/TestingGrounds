16ffabd246636dcf1993c820301a2dc3029ff28a
Sanjyot Dahale <sanjyot.dahale@gmail.com>
2019-08-05 02:19:15 +0530

    1st & 3rd Person
    * Fixed Aiming Angle Issue
    * Renamed PatrollingGuard BP to TPCharacter
    * Using TPCharacter as Player
    * Added FPArms and FPCamera to the TPCharacter BP
    * Using two seperate Guns for TP and FP view
    * FP view for Player and TP view for Enemy
    * Created a seperate FiringComponent on the TPCharacter
    * Bullets are fired using FiringComponent

--- Source/TestingGrounds/Weapons/Gun.cpp
+++ Source/TestingGrounds/Weapons/Gun.cpp
@@ -17,7 +17,7 @@ AGun::AGun()
 	FP_Gun->bCastDynamicShadow = false;
 	FP_Gun->CastShadow = false;
 	// FP_Gun->SetupAttachment(Mesh1P, TEXT("GripPoint"));
-	FP_Gun->SetupAttachment(RootComponent);
+	RootComponent = FP_Gun;
 
 	FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
 	FP_MuzzleLocation->SetupAttachment(FP_Gun);
